b61558198458f87ba7bfa5ebaa4225f3885d437d
Jacob Yoon <jacobyoon926@gmail.com>
2020-01-23 11:27:52 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -84,3 +84,15 @@ onReady(function () {
 $win.on('scroll', function(){
 	//$('#heroImg').css('transform', 'translateY(' + $win.scrollTop()/5 + 'px)');
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
